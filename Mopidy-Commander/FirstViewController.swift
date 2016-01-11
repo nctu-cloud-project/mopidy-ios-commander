ea5f32ca252dfede4d4f00898100f8f53171c2d8
Chih-Yung Liang <stu89424@gmail.com>
2016-01-11 16:44:14 +0800

    Initial Commit

---
+++ Mopidy-Commander/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Mopidy-Commander
+//
+//  Created by Chih-Yung Liang on 2016/1/11.
+//  Copyright © 2016年 Chih-Yung Liang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
